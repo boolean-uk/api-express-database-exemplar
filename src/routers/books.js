d72dc304381c453ae03c276beafb04cb96079c65
phillipdunn <phillipdunn@hotmail.com>
2022-03-25 15:14:32 +0000

    add - request for book by type connected to db

--- src/routers/books.js
+++ src/routers/books.js
@@ -4,10 +4,23 @@ const router = express.Router();
 
 //get /books by type - localhost:3030/books?type=fiction
 router.get('/', (req, res) => {
-  db;
   console.log('got get by id request!', req.query.type);
-
-  res.json({ user: 1 });
+  let selectAllBooksQuery = 'SELECT * FROM books ';
+  let value;
+  if (req.query.type) {
+    selectAllBooksQuery = 'SELECT * FROM books WHERE type = $1';
+    value = [req.query.type];
+  }
+  db.query(selectAllBooksQuery, value)
+    .then((databaseResult) => {
+      console.log('dbresult', databaseResult);
+      res.json({ books: databaseResult.rows });
+    })
+    .catch((error) => {
+      res.status(500);
+      res.json({ error: 'Unexpected Error' });
+      console.log('db error', error);
+    });
 });
 
 module.exports = router;
