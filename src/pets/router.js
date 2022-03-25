6fbb19e1c7ca5b56e392e03b3dc99eb796b410d0
Lewis <ljoncb@gmail.com>
2022-03-25 16:11:47 +0000

    getPetByID route

--- src/pets/router.js
+++ src/pets/router.js
@@ -2,12 +2,21 @@ const express = require("express");
 
 const CreatePetsRouter = (db) => {
   const router = express.Router();
-  const { getAllPets } = createPetsDBFunctions(db);
+  const { getAllPets, getPetByID } = createPetsDBFunctions(db);
 
   router.get("/", async (req, res) => {
     const type = req.query.type || null;
     const pets = await getAllPets(type);
     res.status(200).json({ pets: pets });
+  });
+
+  router.get("/:id", async (req, res) => {
+    const id = req.params.id;
+    const [pet, found] = await getPetByID(id);
+    if (!found) {
+      return res.status(404).json({ error: `pet not found with id: ${id}` });
+    }
+    return res.status(200).json({ pet: pet });
   });
 
   return router;
@@ -31,7 +40,18 @@ const createPetsDBFunctions = (db) => {
     }
   };
 
-  return { getAllPets };
+  const getPetByID = async (id) => {
+    let query = "SELECT * FROM pets WHERE id=$1";
+    try {
+      const res = await db.query(query, [id]);
+      return [res.rows[0], res.rowCount !== 0];
+    } catch (e) {
+      console.error("error getting pet by id:", e.message);
+      return [null, false];
+    }
+  };
+
+  return { getAllPets, getPetByID };
 };
 
 module.exports = CreatePetsRouter;
