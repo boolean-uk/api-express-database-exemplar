2c7831055057b8e561155b5dfd07e35932c15f09
phillipdunn <phillipdunn@hotmail.com>
2022-03-25 16:03:45 +0000

    add - request for book by id

--- src/routers/books.js
+++ src/routers/books.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 //get /books by type - localhost:3030/books?type=fiction
 router.get('/', (req, res) => {
-  console.log('got get by id request!', req.query.type);
+  console.log('got get all request!', req.query.type);
   let selectAllBooksQuery = 'SELECT * FROM books ';
   let value;
   if (req.query.type) {
@@ -14,6 +14,28 @@ router.get('/', (req, res) => {
   db.query(selectAllBooksQuery, value)
     .then((databaseResult) => {
       console.log('dbresult', databaseResult);
+      res.json({ books: databaseResult.rows });
+    })
+    .catch((error) => {
+      res.status(500);
+      res.json({ error: 'Unexpected Error' });
+      console.log('db error', error);
+    });
+});
+
+//localhost:3030/books/3
+router.get('/:id', (req, res) => {
+  console.log('got get by id request!', req.params.id);
+  const selectBooksQuery = 'SELECT * FROM books WHERE id = $1';
+  const value = [req.params.id];
+  db.query(selectBooksQuery, value)
+    .then((databaseResult) => {
+      console.log('dbresult', databaseResult.rowCount);
+      if (databaseResult.rowCount === 0) {
+        res.status(404);
+        res.json({ error: 'id does not exist' });
+        return;
+      }
       res.json({ books: databaseResult.rows });
     })
     .catch((error) => {
